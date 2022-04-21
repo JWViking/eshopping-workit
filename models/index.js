07cc905fc45438c00cad161cc572d369225fcec7
JWViking <julianawilcox@gmail.com>
2022-04-20 19:58:57 -0600

    routes

--- models/index.js
+++ models/index.js
@@ -3,14 +3,31 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const seedTags = require('../seeds/tag-seeds');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', //Is this right?
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id' //Is this right?
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'//Is this right?
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged-products',
+  foreignKey: 'tag_id'// Is this right?
+})
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,41 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Post.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'created_at'],
+    include: [
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      },
+      {
+        model: Category,
+        attributes: ['category_name']
+      }
+    ]
+  });
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock', 'created_at'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
   // be sure to include its associated Category and Tag data
 });
 
@@ -22,10 +51,16 @@ router.post('/', (req, res) => {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
-      tagIds: [1, 2, 3, 4]
+      tagIds: [1, 2, 3, 4]  ***why are tagIds here?***
     }
   */
-  Product.create(req.body)
+    Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock
+    })
+  
+    //Product.create(req.body) ***Why is this here?***
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -91,6 +126,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if(!dbProductData) {
+      res.status(404).json({message: 'No product found with this id.'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
